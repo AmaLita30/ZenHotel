b7d61af72fb456d8dbbfdf203858288839832e6f
AndSyncX <fernandezp.joe@gmail.com>
2025-03-14 14:43:11 -0500

    Fixed: CustomerController

--- src/main/java/idat/edu/pe/ZenHotel/controller/CustomerController.java
+++ src/main/java/idat/edu/pe/ZenHotel/controller/CustomerController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CustomerController {
 
-    @GetMapping("/customer.html")
+    @GetMapping("/customer")
     public String customer() {
         return "customer";
     }
