036b0e120980bf46ee1b8a394af15111f413f831
AmaLita30 <ortiz.chavez.amalia@gmail.com>
2025-03-24 20:27:38 -0500

    feat: LoginModel, LoginController, Login.html

---
+++ src/main/java/idat/edu/pe/ZenHotel/controller/LoginController.java
@@ -0,0 +1,32 @@
+package idat.edu.pe.ZenHotel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.idat.Ec3.model.LoginModel;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String formularioLogin(Model model) {
+        model.addAttribute("loginmodel", new LoginModel());
+        return "login"; // Nombre de la vista HTML del formulario de login
+    }
+
+    @PostMapping("/acceso")
+    public String procesarLogin(@ModelAttribute("loginmodel") LoginModel login, Model model) {
+        String usuarioValido = "admin"; // Usuario válido (puedes cambiarlo)
+        String claveValida = "1234";    // Contraseña válida
+
+        if (login.getUser().equals(usuarioValido) && login.getPassword().equals(claveValida)) {
+            model.addAttribute("mensaje", "¡Inicio de sesión exitoso!");
+            return "Bienvenido(a)"; // Vista a la que redirigirás si el login es correcto
+        } else {
+            model.addAttribute("Error!", "Usuario o contraseña incorrectos");
+            return "login"; // Si falla, vuelve al formulario de login
+        }
+    }
+}
