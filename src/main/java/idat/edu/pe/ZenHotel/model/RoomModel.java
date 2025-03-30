d216e84ead223639c2863bd49ce081ccf1b61022
AndSyncX <fernandezp.joe@gmail.com>
2025-03-30 17:11:24 -0500

    Feat: PersonModel

--- src/main/java/idat/edu/pe/ZenHotel/model/RoomModel.java
+++ src/main/java/idat/edu/pe/ZenHotel/model/RoomModel.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 public class RoomModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer idroom;
     private Integer roomnum;
     private Double price;
     private String roomdescription;
@@ -17,51 +17,5 @@ public class RoomModel {
     @JoinColumn(name = "idroomstatus")
     private RoomStatusModel roomstatus;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getRoomnum() {
-        return roomnum;
-    }
-
-    public void setRoomnum(Integer roomnum) {
-        this.roomnum = roomnum;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getRoomdescription() {
-        return roomdescription;
-    }
-
-    public void setRoomdescription(String roomdescription) {
-        this.roomdescription = roomdescription;
-    }
-
-    public RoomTypeModel getRoomtype() {
-        return roomtype;
-    }
-
-    public void setRoomtype(RoomTypeModel roomtype) {
-        this.roomtype = roomtype;
-    }
-
-    public RoomStatusModel getRoomstatus() {
-        return roomstatus;
-    }
-
-    public void setRoomstatus(RoomStatusModel roomstatus) {
-        this.roomstatus = roomstatus;
-    }
 }
