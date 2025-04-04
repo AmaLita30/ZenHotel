1cac7af852b4a997b644154d623c99f59a610c8d
AndSyncX <fernandezp.joe@gmail.com>
2025-04-04 00:00:04 -0500

    Feat: Actualizacion Persona, Cliente

--- src/main/java/idat/edu/pe/ZenHotel/model/EmployeeModel.java
+++ src/main/java/idat/edu/pe/ZenHotel/model/EmployeeModel.java
@@ -3,7 +3,7 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "employee")
-public class  {
+public class EmployeeModel{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idemployee;
