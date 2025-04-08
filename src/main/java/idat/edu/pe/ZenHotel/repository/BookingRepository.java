28f456f38754602f96b50b02bff5a77f2d98226c
AmaLita30 <ortiz.chavez.amalia@gmail.com>
2025-04-08 14:48:41 -0500

    Feat: Booking: Conectar Datos

--- src/main/java/idat/edu/pe/ZenHotel/repository/BookingRepository.java
+++ src/main/java/idat/edu/pe/ZenHotel/repository/BookingRepository.java
@@ -2,5 +2,29 @@ package idat.edu.pe.ZenHotel.repository;
 
 import idat.edu.pe.ZenHotel.model.BookingModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface BookingRepository extends JpaRepository<BookingModel, Integer> { }
+import java.util.Date;
+
+
+public interface BookingRepository extends JpaRepository<BookingModel, Integer> {
+    @Query(value = """
+            UPDATE booking SET
+            idbooking = :book,
+            bookingdate = :bookingdate:
+            checkindate = :checkindate:
+            checkoutdate = :checkoutdate
+            idroom = :idroom
+            idcustomer = :idcustomer
+            WHERE idbooking = :idbooking
+            """, nativeQuery = true)
+    void updateParcialEmployee(
+            @Param("idbooking") Integer idbooking,
+            @Param("bookingdate") Date bookingdate,
+            @Param("checkindate") Date checkindate,
+            @Param("checkoutdate") Date checkoutdate,
+            @Param("idroom") Integer idroom,
+            @Param("idcustomer") Integer idcustomer
+    );
+}
